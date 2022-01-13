fd6524bc2357c3c8a5f89ec0e3645b019eab93ac
Sabrina <sabrinaschmidt335@gmail.com>
2022-01-13 18:41:30 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,9 +1,34 @@
+import MovieCards from 'components/MovieCards';
+import MovieScore from 'components/MovieScore';
+import MovieStars from 'components/MovieStars';
 import Pagination from 'components/Pagination';
 import React, { Component } from 'react';
 
 function Listing() {
     return (
-        <Pagination/>
+        <>
+            <Pagination />
+
+            <div className='container'>
+                <div className='row'>
+                    <div className='col-sm-6 col-lg-4 col-xl-3 mb-3'>
+                        <MovieCards />
+                    </div>
+
+                    <div className='col-sm-6 col-lg-4 col-xl-3 mb-3'>
+                        <MovieCards />
+                    </div>
+
+                    <div className='col-sm-6 col-lg-4 col-xl-3 mb-3'>
+                        <MovieCards />
+                    </div>
+
+                    <div className='col-sm-6 col-lg-4 col-xl-3 mb-3'>
+                        <MovieCards />
+                    </div>
+                </div>
+            </div>
+        </>
     );
 }
 
