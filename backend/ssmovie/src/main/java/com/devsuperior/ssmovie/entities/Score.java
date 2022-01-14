e85d72c5e0f4ae9a02dc27c08a2a92f0bbd65ca5
Sabrina <sabrinaschmidt335@gmail.com>
2022-01-14 13:44:18 -0300

    Domain model, database seed

---
+++ backend/ssmovie/src/main/java/com/devsuperior/ssmovie/entities/Score.java
@@ -0,0 +1,58 @@
+/**
+ * 
+ */
+package com.devsuperior.ssmovie.entities;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * @author sabrina
+ *
+ */
+
+@Entity
+@Table(name = "tb_score")
+public class Score {
+
+	@EmbeddedId
+	private ScorePk id = new ScorePk();
+	private Double value;
+
+	public Score() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public ScorePk getId() {
+		return id;
+	}
+
+	public void setId(ScorePk id) {
+		this.id = id;
+	}
+
+	public Double getValue() {
+		return value;
+	}
+
+	public void setValue(Double value) {
+		this.value = value;
+	}
+
+	public void setMovie(Movie movie) {
+		id.setMovie(movie);
+
+	}
+
+	public void setUser(User user) {
+		id.setUser(user);
+
+	}
+
+	@Override
+	public String toString() {
+		return "Score [id=" + id + ", value=" + value + "]";
+	}
+
+}
